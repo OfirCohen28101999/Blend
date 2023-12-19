c199c444b6d0298b35c03ca73e9983b964c83523
Shirel Pourostamian <shirelpo@gmail.com>
2023-12-19 20:18:30 +0200

    first commit

---
+++ src/components/signIn/signIn.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import './signIn.css';
+// import './signIn.html'
+interface SignInProps {}
+
+const SignIn: FC<SignInProps> = () => (
+<div className="signIn">
+  SignIn Component
+  </div>);
+
+export default SignIn;
